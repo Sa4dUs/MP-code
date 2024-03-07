eca1917508e0438cf5507a081bf9ee9f53eab362
Marcelo Domínguez <69964857+Sa4dUs@users.noreply.github.com>
2024-03-07 17:29:59 +0100

    api class initialization

---
+++ src/main/java/api/CharacterHandler.java
@@ -0,0 +1,11 @@
+package api;
+
+import lib.RequestBody;
+import lib.ResponseBody;
+
+public class CharacterHandler {
+    public ResponseBody request(String endpoint, RequestBody body) {
+
+        return null;
+    }
+}
--- src/main/java/api/Handler.java
+++ src/main/java/api/Handler.java
@@ -1,4 +1,11 @@
 package api;
 
-public class Handler {
+import server.Service;
+import lib.RequestBody;
+import lib.ResponseBody;
+
+public interface Handler {
+    Service service = null;
+
+    public ResponseBody request(String endpoint, RequestBody body);
 }

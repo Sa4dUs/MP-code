1a761be1623ff45b0941c826c686d928af655371
Marcelo Domínguez <69964857+Sa4dUs@users.noreply.github.com>
2024-03-10 19:01:31 +0100

    jorge es retraído parte 2

--- src/main/java/api/RequestHandler.java
+++ src/main/java/api/RequestHandler.java
@@ -5,22 +5,12 @@ import lib.RequestBody;
 import lib.ResponseBody;
 import lib.Route;
 
-<<<<<<< Updated upstream
-public class RequestHandler extends Handler<Function<RequestBody, ResponseBody>> {
-    private final Map<String, Handler<Function<RequestBody, ResponseBody>>> endpointHandlers = new HashMap<>();
-
-    public RequestHandler() {
-        endpointHandlers.put(Endpoint.AUTH, new AutenticationHandler());
-        endpointHandlers.put(Endpoint.CHARACTER, new CharacterHandler());
-        endpointHandlers.put(Endpoint.CHALLENGE, new ChallengeHandler());
-=======
 public class RequestHandler extends Handler<Handler<NFunction<ResponseBody>>> {
 
     public RequestHandler() {
         this.operations.put(Endpoint.AUTH, new AuthenticationHandler());
         this.operations.put(Endpoint.CHARACTER, new CharacterHandler());
         this.operations.put(Endpoint.CHALLENGE, new ChallengeHandler());
->>>>>>> Stashed changes
     }
 
     public ResponseBody request(String endpoint, RequestBody body) {
@@ -28,11 +18,7 @@ public class RequestHandler extends Handler<Handler<NFunction<ResponseBody>>> {
 
         Route route = new Route(endpoint);
 
-<<<<<<< Updated upstream
-        Handler<Function<RequestBody, ResponseBody>> handler = endpointHandlers.getOrDefault(route.pop(), null);
-=======
         Handler<NFunction<ResponseBody>> handler = this.operations.getOrDefault(route.pop(), null);
->>>>>>> Stashed changes
 
         return handler == null ? null : handler.request(route.pop(), body);
     }

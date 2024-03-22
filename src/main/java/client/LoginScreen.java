bf998c678b24eafbbb1d440b78a356e240a8041a
shania <s.manso.2022@alumnos.urjc.es>
2024-03-22 01:55:48 +0100

    ui update

--- src/main/java/client/LoginScreen.java
+++ src/main/java/client/LoginScreen.java
@@ -1,4 +1,10 @@
 package client;
 
+import javax.swing.*;
+
 public class LoginScreen implements Screen{
+    private JButton logInButton;
+    private JTextField Username;
+    private JPasswordField Password;
+
 }

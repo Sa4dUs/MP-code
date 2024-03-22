983fc0ed93a5f292efe457ac36e91e1d8d5ba7e3
shania <s.manso.2022@alumnos.urjc.es>
2024-03-22 20:18:24 +0100

    send challenge ui done

--- src/main/java/client/SendChallengeScreen.java
+++ src/main/java/client/SendChallengeScreen.java
@@ -1,4 +1,9 @@
 package client;
 
+import javax.swing.*;
+
 public class SendChallengeScreen implements Screen{
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton challengeButton;
 }

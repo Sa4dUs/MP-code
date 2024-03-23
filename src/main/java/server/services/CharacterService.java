643bfc8dff3cfa16a847aa0478fe31762ceebb53
Marcelo Domínguez <69964857+Sa4dUs@users.noreply.github.com>
2024-03-23 12:13:32 +0100

    major code changes

--- src/main/java/server/services/CharacterService.java
+++ src/main/java/server/services/CharacterService.java
@@ -6,13 +6,13 @@ import server.characters.Character;
 public class CharacterService implements Service {
     public ResponseBody createCharacter(Character character) {
         return new ResponseBody(true);
-    };
+    }
 
     public ResponseBody updateCharacter(String id, Character character) {
         return new ResponseBody(true);
-    };
+    }
 
     public ResponseBody deleteCharacter(String id) {
         return new ResponseBody(true);
-    };
+    }
 }

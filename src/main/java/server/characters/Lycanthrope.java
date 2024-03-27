5effb3d81cc56710bd1418e3f4f3d97b1529e02c
shania <s.manso.2022@alumnos.urjc.es>
2024-03-27 01:19:06 +0100

    tick method

--- src/main/java/server/characters/Lycanthrope.java
+++ src/main/java/server/characters/Lycanthrope.java
@@ -5,10 +5,10 @@ public class Lycanthrope extends FightCharacter{
 
     public Lycanthrope(PlayerCharacter character) {
         super(character);
+        this.maxMana = 3;
+        setMana(maxMana);
     }
 
     @Override
-    public void tick() {
-
-    }
+    public void tick() {this.setMana(Math.min(this.getMana() + 1, maxMana)); }
 }
--- src/main/java/server/characters/Vampire.java
+++ src/main/java/server/characters/Vampire.java
@@ -5,10 +5,10 @@ public class Vampire extends FightCharacter{
 
     public Vampire(PlayerCharacter character) {
         super(character);
+        this.maxMana = 10;
+        setMana(maxMana);
     }
 
     @Override
-    public void tick() {
-
-    }
+    public void tick() {this.setMana(Math.min(this.getMana() + 1, maxMana)); }
 }

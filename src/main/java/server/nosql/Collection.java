2c39e4f6da93a482d5205c56081fb54d3739e834
Lamoara <jorginos40@gmail.com>
2024-03-28 23:52:09 +0100

    Changed stuff

--- src/main/java/server/nosql/Collection.java
+++ src/main/java/server/nosql/Collection.java
@@ -4,5 +4,7 @@ public class Collection {
     public static final String USER = "users-test";
     public static final String CHALLENGE = "challenges-test";
     public static final String CHALLENGE_OPERATORS = "challenges-operators-test";
-    public static final String DEFAULT_CHARACTER = "characters-test";
+    public static final String DEFAULT_CHARACTER = "characters-default-test";
+    public static final String CHARACTER = "characters-test";
+    public static final String ITEMS = "items-test";
 }
--- src/main/java/server/services/ChallengeService.java
+++ src/main/java/server/services/ChallengeService.java
@@ -7,14 +7,19 @@ import server.characters.Character;
 import server.characters.PlayerCharacter;
 import server.items.Ability;
 import server.items.Armor;
+import server.items.Stats;
 import server.items.Weapon;
+import server.minions.Minion;
 import server.nosql.Collection;
 import server.nosql.Document;
 import server.nosql.Query;
 import server.nosql.Schemas.ChallengeRequestSchema;
 import server.nosql.Schemas.ChallengeResultSchema;
 
+import javax.xml.crypto.Data;
 import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
 import java.util.*;
 
 public class ChallengeService implements Service {
@@ -71,38 +76,71 @@ public class ChallengeService implements Service {
             return null;
 
         PlayerCharacter character = getPlayerCharacter((String) doc.getProperty("playerCharacterId"));
-
-        return new Player(doc);
+        Player player = new Player(doc);
+        player.setCharacter(character);
+        return player;
     }
 
-    private Character getCharacter(String id)
-    {
-        return null;
-    }
+    //private Character getCharacter(String id)
+    //{
+    //    return null;
+    //}
 
     private PlayerCharacter getPlayerCharacter(String id)
     {
-        return null;
+        Query query = new Query();
+        query.addFilter("id", id);
+        Document doc = Database.findOne(Collection.CHARACTER, query);
+        if(doc == null)
+            return null;
+
+        try {
+            doc.setProperty("weaponsList", getItemArray((String[]) doc.getProperty("weaponId"), Weapon.class));
+            doc.setProperty("armorList", getItemArray((String[]) doc.getProperty("armorId"), Armor.class));
+            doc.setProperty("abilityList", getItemArray((String[]) doc.getProperty("abilityId(N)"), Ability.class));
+            doc.setProperty("specialAbilityList", getItemArray((String[]) doc.getProperty("abilityId(S)"), Ability.class));
+            doc.setProperty("debilitiesList", getItemArray((String[]) doc.getProperty("characteristicId(D)"), Characteristic.class));
+            doc.setProperty("resistancesList", getItemArray((String[]) doc.getProperty("characteristicId(S)"), Characteristic.class));
+            //doc.setProperty("armorList", getItemArray((String[]) doc.getProperty("minionId"), Minion.class));
+
+            doc.setProperty("activeWeaponL", getItem((String) doc.getProperty("weaponId(LWeapon)"), Weapon.class));
+            doc.setProperty("activeWeaponR", getItem((String) doc.getProperty("weaponId(RWeapon)"), Weapon.class));
+            doc.setProperty("activeArmor", getItem((String) doc.getProperty("armorId(Active)"), Armor.class));
+            doc.setProperty("activeNormalAbility", getItem((String) doc.getProperty("abilityId(ActiveN)"), Ability.class));
+            doc.setProperty("activeSpecialAbility", getItem((String) doc.getProperty("abilityId(ActiveS)"), Ability.class));
+        } catch (Exception e) {
+            return null;
+        }
+        return new PlayerCharacter(doc);
     }
 
-    private Weapon getWeapon(String id)
-    {
-        return null;
+
+    private Stats[] getItemArray(String[] ids, Class<?> clazz){
+        List<Stats> list = new ArrayList<>();
+
+        for (String id: ids)
+        {
+            Query query = new Query();
+            query.addFilter("id", id);
+            Document doc = Database.findOne(Collection.ITEMS, query);
+            try {
+                list.add((Stats) clazz.getDeclaredConstructor(Document.class).newInstance(doc));
+            } catch (Exception e) {
+                continue;
+            }
+        }
+        return list.toArray(new Stats[0]);
     }
 
-    private Armor getArmor(String id)
-    {
-        return null;
-    }
-
-    private Ability getAbility(String id)
-    {
-        return null;
-    }
-
-    private Characteristic getCharacteristic(String id)
-    {
-        return null;
+    private Stats getItem(String id, Class<?> clazz) {
+        Query query = new Query();
+        query.addFilter("id", id);
+        Document doc = Database.findOne(Collection.ITEMS, query);
+        try {
+            return (Stats) clazz.getDeclaredConstructor(Document.class).newInstance(doc);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 

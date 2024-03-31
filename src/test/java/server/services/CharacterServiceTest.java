ba553ef61cc77463958a1cd7ecbe27e8be673d73
Lamoara <jorginos40@gmail.com>
2024-03-31 12:46:13 +0200

    Character Service tests

---
+++ src/test/java/server/services/CharacterServiceTest.java
@@ -0,0 +1,47 @@
+package server.services;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import server.Player;
+import server.characters.Character;
+import server.characters.CharacterType;
+import server.characters.PlayerCharacter;
+import server.nosql.Document;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterServiceTest {
+
+    @Test
+    public void generateCharacterTest()
+    {
+        Character character = new Character();
+        character.setBreed(CharacterType.Hunter);
+        character.setName("Vayne");
+        character.setHealth(3);
+        character.setGold(500);
+
+        CharacterService service = new CharacterService();
+        service.createCharacter(character);
+
+        Character character1 = (Character) Document.getDocument(character.getId(), Character.class).deJSONDocument(Character.class);
+        Assert.assertEquals(character1.getName(), character.getName());
+    }
+
+    @Test
+    public void generatePlayerCharacterTest()
+    {
+        PlayerCharacter character = new PlayerCharacter();
+        character.setBreed(CharacterType.Hunter);
+        character.setName("Juan");
+        character.setHealth(3);
+        character.setGold(500);
+
+        CharacterService service = new CharacterService();
+        service.createCharacter(character);
+
+        PlayerCharacter character1 = (PlayerCharacter) Document.getDocument(character.getId(), PlayerCharacter.class).deJSONDocument(PlayerCharacter.class);
+        Assert.assertEquals(character1.getName(), character.getName());
+    }
+
+}

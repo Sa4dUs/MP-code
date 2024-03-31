d1215f1fa26eadd9a0525ba2f549d8d6bc05681f
Lamoara <jorginos40@gmail.com>
2024-03-31 14:41:30 +0200

    Challenge service test

--- src/test/java/server/services/ChallengeServiceTest.java
+++ src/test/java/server/services/ChallengeServiceTest.java
@@ -1,24 +1,32 @@
 package server.services;
 
-import lib.ResponseBody;
 import org.junit.jupiter.api.Test;
-import server.Database;
+import server.ChallengeRequest;
+import server.ChallengeResult;
 import server.Player;
-import server.User;
-import server.characters.Character;
-import server.characters.CharacterType;
-import server.characters.PlayerCharacter;
-import server.items.Armor;
-import server.items.Weapon;
-import server.minions.Demon;
-import server.minions.Human;
-import server.minions.Minion;
 import server.nosql.Document;
-import server.nosql.Query;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChallengeServiceTest {
 
+    @Test
+    public void createResultTest()
+    {
+        Player p1 = (Player) Document.getDocument("L60YY", Player.class).deJSONDocument(Player.class);
+        Player p2 = (Player) Document.getDocument("L28SS", Player.class).deJSONDocument(Player.class);
+        Player p3 = (Player) Document.getDocument("C15PJ", Player.class).deJSONDocument(Player.class);
+        ChallengeResult result = new ChallengeResult(p1, p2, 50);
+        ChallengeResult result1 = new ChallengeResult(p2, p1, 69);
+        ChallengeResult result2 = new ChallengeResult(p3, p2, 100);
+        ChallengeResult result3 = new ChallengeResult(p3, p1, 500);
+
+        result.getDocument().saveToDatabase(ChallengeResult.class);
+        result1.getDocument().saveToDatabase(ChallengeResult.class);
+        result2.getDocument().saveToDatabase(ChallengeResult.class);
+        result3.getDocument().saveToDatabase(ChallengeResult.class);
+
+
+    }
 
 }

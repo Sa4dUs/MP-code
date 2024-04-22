d0467b4e1625c58499c63392af6f879da69aa3c7
Marcelo Domínguez <69964857+Sa4dUs@users.noreply.github.com>
2024-04-22 16:14:40 +0200

    test signup route

--- src/test/java/api/AuthenticationHandlerTest.java
+++ src/test/java/api/AuthenticationHandlerTest.java
@@ -19,6 +19,12 @@ class AuthenticationHandlerTest {
 
     @Test
     void signup() {
+        RequestHandler handler = new RequestHandler();
 
+        RequestBody data = new RequestBody();
+        data.addField("username", "user");
+        data.addField("password", "1234");
+
+        assertNotNull(handler.request("auth/signup", data));
     }
 }

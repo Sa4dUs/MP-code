55934297c89d30319754d96966061e028706f786
shania <s.manso.2022@alumnos.urjc.es>
2024-04-28 21:41:42 +0200

    challengeServiceTest (not completed)

--- src/test/java/server/services/ChallengeServiceTest.java
+++ src/test/java/server/services/ChallengeServiceTest.java
@@ -1,8 +1,10 @@
 package server.services;
 
+import lib.ResponseBody;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import server.ChallengeRequest;
 import server.Player;
 import server.characters.Character;
 import server.characters.PlayerCharacter;
@@ -11,21 +13,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ChallengeServiceTest {
 
-    private CharacterService characterService;
-    private static Player player;
-    private static PlayerCharacter character;
+    private ChallengeService challengeService;
+    public static Player player1, player2;
+    public static PlayerCharacter playerCharacter1, playerCharacter2;
+
     @BeforeEach
     void setUp() {
-        characterService = new CharacterService();
+        challengeService = new ChallengeService();
 
-        character = new PlayerCharacter();
-        character.setId("00000000-0000-0000-0000-000000000000");
-        character.getDocument().saveToDatabase(character.getClass());
+        playerCharacter1 = new PlayerCharacter();
+        playerCharacter1.setId("00000000-0000-0000-0000-000000000000");
+        playerCharacter1.setGold(500);
+        playerCharacter1.getDocument().saveToDatabase(PlayerCharacter.class);
 
-        player = new Player();
-        player.setId("A12BC");
-        player.setCharacter(character);
-        player.getDocument().saveToDatabase(player.getClass());
+        player1 = new Player();
+        player1.setId("A12BC");
+        player1.setCharacter(playerCharacter1);
+        player1.getDocument().saveToDatabase(player1.getClass());
+
+        playerCharacter2 = new PlayerCharacter();
+        playerCharacter2.setId("00000000-0000-0000-0000-000000000001");
+        playerCharacter2.setGold(500);
+        playerCharacter2.getDocument().saveToDatabase(PlayerCharacter.class);
+
+        player2 = new Player();
+        player2.setId("D13EF");
+        player2.setCharacter(playerCharacter2);
+        player2.getDocument().saveToDatabase(player2.getClass());
     }
 
     @AfterEach
@@ -33,61 +47,72 @@ class ChallengeServiceTest {
     }
 
     @Test
-    void createCharacter() {
-        Character character = new Character();
-        assertTrue(characterService.createCharacter(character).ok);
+    void testCreateChallenge() {
+        ChallengeRequest challengeRequest = new ChallengeRequest("A12BC", "D13EF", 100);
+        ResponseBody response = challengeService.createChallenge(challengeRequest);
+        assertTrue(response.ok);
+    }
+
+
+    @Test
+    void testGetChallenge() {
+        ChallengeRequest challengeRequest = new ChallengeRequest("A12BC", "D13EF", 100);
+        String id = challengeRequest.getId();
+        ResponseBody response = challengeService.getChallenge(id);
+        assertTrue(response.ok);
     }
 
     @Test
-    void createPlayerCharacter() {
-        PlayerCharacter playerCharacter = new PlayerCharacter();
-        assertTrue(characterService.createPlayerCharacter(playerCharacter).ok);
+    void testAcceptChallengeFromPlayer() {
+        ChallengeRequest challengeRequest = new ChallengeRequest("A12BC", "D13EF", 100);
+        ResponseBody response = challengeService.acceptChallengeFromPlayer(challengeRequest);
+        assertTrue(response.ok);
     }
 
     @Test
-    void setCharacterOfPlayer() {
-        assertTrue(characterService.setCharacterOfPlayer(player.getNick(), character).ok);
+    void testAcceptChallengeFromOperator() {
+        ChallengeRequest challengeRequest = new ChallengeRequest("A12BC", "D13EF", 100);
+        ResponseBody response = challengeService.acceptChallengeFromOperator(challengeRequest);
+        assertTrue(response.ok);
     }
 
     @Test
-    void updateCharacter() {
-        Character character = new Character(/* Initialize character data */);
-        assertTrue(characterService.updateCharacter(character).ok);
+    void testDenyChallengeFromPlayer() {
+        ChallengeRequest challengeRequest = new ChallengeRequest("A12BC", "D13EF", 100);
+        ResponseBody response = challengeService.denyChallengeFromPlayer(challengeRequest);
+        assertTrue(response.ok);
     }
 
     @Test
-    void updatePlayerCharacter() {
-        assertTrue(characterService.updatePlayerCharacter(character).ok);
+    void testAddIdToPlayer() {
+        String challengeId = "12345678";
+        ResponseBody response = challengeService.addIdToPlayer(challengeId, player1.getNick(), "pendingDuels");
+        assertTrue(response.ok);
+
     }
 
     @Test
-    void deleteCharacter() {
-        assertTrue(characterService.deleteCharacter(character.getId(), Character.class).ok);
+    void testRemoveIdFromPlayer() {
+        ChallengeRequest challengeRequest = new ChallengeRequest("A12BC", "D13EF", 100);
+        String challengeId = "12345678";
+        challengeRequest.setId(challengeId);
+
+        player1.addPending(challengeRequest);
+        player1.getDocument().saveToDatabase(Player.class);
+        ResponseBody response = challengeService.removeIdFromPlayer(challengeId, player1.getNick(), "pendingDuels");
+        assertTrue(response.ok);
+    }
+
+
+    @Test
+    void testGetRanking() {
+        ResponseBody response = challengeService.getRanking();
+        assertTrue(response.ok);
     }
 
     @Test
-    void getDefaultCharacters() {
-        assertNotNull(characterService.getDefaultCharacters().getField("characterList"));
-    }
-
-    @Test
-    void getPlayerCharacters() {
-        assertNotNull(characterService.getPlayerCharacters().getField("characterList"));
-    }
-
-    @Test
-    void setIdToPlayer() {
-        assertTrue(characterService.setIdToPlayer(character.getId(), player.getNick(), "character").ok);
-    }
-
-    /*
-    void removeIdFromPlayer() {
-        assertTrue(characterService.removeIdFromPlayer(character.getId(), player.getNick(), "character").ok);
-    }
-    */
-
-    @Test
-    void getCharacterFromPlayerNick() {
-        assertNotNull(characterService.getCharacterFromPlayerNick(player.getNick()).getField("character"));
+    void testGetOperatorChallenges() {
+        ResponseBody response = challengeService.getOperatorChallenges();
+        assertTrue(response.ok);
     }
 }

c63b1341492ccbfb3cc2ea9bf006212257d3929d
Marcelo Domínguez <69964857+Sa4dUs@users.noreply.github.com>
2024-05-02 00:50:20 +0200

    added some tests

---
+++ src/test/java/server/CryptoTest.java
@@ -0,0 +1,33 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CryptoTest {
+
+    String key = "Jorge usó explosión!";
+
+    @Test
+    void testUUIDv4() {
+        assertNotEquals(Crypto.UUIDv4(), "");
+    }
+
+    @Test
+    void testEncryptDecrypt() {
+        String encrypted = Crypto.encrypt(key);
+        assertNotNull(encrypted);
+
+        String decrypted = Crypto.decrypt(encrypted);
+        assertNotNull(decrypted);
+        assertEquals(key, decrypted);
+    }
+
+    @Test
+    void testCompare() {
+        String encrypted = Crypto.encrypt(key);
+
+        assertTrue(Crypto.compare(encrypted, key));
+        assertFalse(Crypto.compare(encrypted, key + "a"));
+    }
+}
